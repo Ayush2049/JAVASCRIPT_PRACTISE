a95eeb625563a0e492a0201e7d1b9ec4aba26406
Ayush sharma <163329164+Ayush2049@users.noreply.github.com>
2025-10-18 20:55:54 +0530

    Add files via upload

---
+++ 4_Arrow_Function/4 arrow_Function.js
@@ -0,0 +1,18 @@
+// traditional function
+function add(a, b) {
+  return a + b;
+}
+
+const functionName = (param1, param2) => {
+  // Function body
+  return param1 + param2;
+};
+console.log(functionName(1, 2));
+
+// arrow function
+
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(3, 5));
